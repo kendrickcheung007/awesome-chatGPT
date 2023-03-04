6e53dffa1431496e2983216099c76b2c46cabd34
kendrickcheung <supporthuawei2021@163.com>
2023-03-04 16:15:07 +0800

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,10 @@
 import { defineConfig } from 'astro/config'
+import vercel from '@astrojs/vercel/edge'
 import unocss from 'unocss/astro'
 import { presetUno } from 'unocss'
 import presetAttributify from '@unocss/preset-attributify'
 import presetTypography from '@unocss/preset-typography'
 import solidJs from '@astrojs/solid-js'
-import vercelDisableBlocks from './plugins/vercelDisableBlocks'
-
-import node from '@astrojs/node'
-import vercel from '@astrojs/vercel/edge'
-
-const envAdapter = () => {
-  if (process.env.OUTPUT == 'vercel') {
-    return vercel()
-  } else {
-    return node({
-      mode: 'standalone'
-    })
-  }
-}
 
 // https://astro.build/config
 export default defineConfig({
@@ -32,10 +19,5 @@ export default defineConfig({
     solidJs()
   ],
   output: 'server',
-  adapter: envAdapter(),
-  vite: {
-    plugins: [
-      process.env.OUTPUT == 'vercel' && vercelDisableBlocks(),
-    ]
-  },
+  adapter: vercel()
 });
